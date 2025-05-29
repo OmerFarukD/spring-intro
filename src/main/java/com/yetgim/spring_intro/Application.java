bc10bf28c9e632b851fb6e89e04842752bef6567
Pendik <omer.crlys.44@gmail.com>
2025-05-29 16:56:30 +0300

    Initial commit

---
+++ src/main/java/com/yetgim/spring_intro/Application.java
@@ -0,0 +1,14 @@
+package com.yetgim.spring_intro;
+
+import com.yetgim.spring_intro.entities.Product;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/yetgim/spring_intro/entities/Product.java
@@ -0,0 +1,36 @@
+package com.yetgim.spring_intro.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long id;
+
+
+    @Column
+    private String name;
+
+    @Column
+    private double price;
+
+    @Column
+    private int stock;
+
+
+
+}
